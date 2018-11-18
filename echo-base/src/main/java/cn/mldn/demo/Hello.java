58440c23898c118e94f6b7e0fecac5ecf50ffc8d
yootk <784420216@qq.com>
2018-11-18 08:36:26 +0800

    创建基本的Echo程序项目，按照传统开发（base）、netty、常用工具类进行模块拆分。

---
+++ echo-base/src/main/java/cn/mldn/demo/Hello.java
@@ -0,0 +1,7 @@
+package cn.mldn.demo;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.printf("www.mldn.cn");
+    }
+}
---
+++ echo-netty/src/main/java/cn/mldn/App.java
@@ -0,0 +1,13 @@
+package cn.mldn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
